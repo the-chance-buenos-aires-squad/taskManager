63d556bc8c620c266364a1a853cf950649b6a5d5
Moyasser Hazim <58323188+moyasser-hazim@users.noreply.github.com>
2025-04-28 17:53:24 +0300

    Task/3 add entities (#4)
    Co-authored-by: Falah Hassan <68475489+devfalah@users.noreply.github.com>

---
+++ src/main/kotlin/domain/entities/User.kt
@@ -0,0 +1,11 @@
+package org.buinos.domain.entities
+
+import java.time.LocalDateTime
+
+data class User(
+    val id: String,
+    val username: String,
+    val password: String,
+    val role: UserRole,
+    val createdAt: LocalDateTime
+)
