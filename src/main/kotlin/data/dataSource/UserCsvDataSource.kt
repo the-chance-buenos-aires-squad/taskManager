b0603752f01ba0fede30d1fa250d1a0eaf4ed3a5
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-04-29 11:34:40 +0300

    add ensure file existence method

--- src/main/kotlin/data/dataSource/UserCsvDataSource.kt
+++ src/main/kotlin/data/dataSource/UserCsvDataSource.kt
@@ -1,8 +1,14 @@
 package data.dataSource
 
+import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
 import org.buinos.domain.entities.User
+import java.io.File
 
-class UserCsvDataSource : DataSource<User>{
+class UserCsvDataSource : DataSource<User> {
+
+    init {
+        createFileWithHeaders()
+    }
 
     override fun insertItem(item: User): Boolean {
         TODO("Not yet implemented")
@@ -22,6 +28,31 @@ class UserCsvDataSource : DataSource<User>{
 
     override fun updateItem(item: User): Boolean {
         TODO("Not yet implemented")
+    }
+
+
+    private fun createFileWithHeaders() {
+        val file = File(usersFilePath)
+
+        if (!file.exists()) {
+            val isCreated = file.createNewFile()
+
+            if (isCreated) {
+                val userHeader: List<String> = listOf("id", "username", "password", "role", "createdAt")
+                csvWriter().open(file) {
+                    writeRow(userHeader)
+                }
+                println("File created and header written: $userHeader")
+            } else {
+                println("Failed to create file")
+            }
+        } else {
+            println("File already exists.")
+        }
+    }
+
+    companion object {
+        const val usersFilePath = "src/main/kotlin/data/resource/users_file.csv"
     }
 
 }
