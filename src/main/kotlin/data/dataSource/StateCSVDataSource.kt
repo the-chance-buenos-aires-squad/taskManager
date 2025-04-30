88e6540f98183ef7d6b595185bb267799da7d83f
NadeenRR <nadeenradwan0@gmail.com>
2025-04-30 14:07:14 +0300

    - add fun in StateCSVDataSource

--- src/main/kotlin/data/dataSource/StateCSVDataSource.kt
+++ src/main/kotlin/data/dataSource/StateCSVDataSource.kt
@@ -1,10 +1,19 @@
 package data.dataSource
 
+import org.buinos.domain.entities.State
 import java.io.File
 
 class StateCSVDataSource(private val filePath: String = "kotlin/data/resource/state.csv") {
 
     private val file = File(filePath)
+
+    fun getAllStates(): List<State> {
+        TODO("Not yet implemented")
+    }
+
+    fun updateState(updatedState: State): Boolean {
+        TODO("Not yet implemented")
+    }
 }
 
 
