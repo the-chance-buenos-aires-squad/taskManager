e637c7ad158789c88364ae0f5e00e9009f698496
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-05-01 07:19:27 +0300

    write test case for usecase create user

---
+++ src/test/kotlin/domain/usecases/CreateUserUseCaseTest.kt
@@ -0,0 +1,109 @@
+package domain.usecases
+
+import domain.customeExceptions.InvalidConfirmPasswordException
+import domain.customeExceptions.InvalidLengthPasswordException
+import domain.customeExceptions.UserAlreadyExistException
+import domain.customeExceptions.UserNameEmptyException
+import domain.repositories.UserRepository
+import dummyData.DummyUser
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class CreateUserUseCaseTest {
+    private var userRepository: UserRepository = mockk(relaxed = true)
+    private val createUserUseCase = CreateUserUseCase(userRepository)
+    val firstUser = DummyUser.testUserOne
+    val secondUser = DummyUser.testUserTwo
+
+    @Test
+    fun `should create admin successfully`() {
+        //given
+        every { userRepository.getUserByUserName(firstUser.username) } returns null
+
+        //when
+        createUserUseCase.createUser(
+            username = firstUser.username,
+            password = firstUser.password,
+            confirmPassword = firstUser.password,
+            userRole = firstUser.role
+        )
+
+        //then
+        verify(exactly = 1) {
+            userRepository.insertUser(firstUser)
+        }
+
+    }
+
+    @Test
+    fun `should create mate successfully`() {
+        //given
+        every { userRepository.getUserByUserName(secondUser.username) } returns null
+
+        //when
+        createUserUseCase.createUser(
+            username = secondUser.username,
+            password = secondUser.password,
+            confirmPassword = secondUser.password,
+            userRole = secondUser.role
+        )
+
+        //then
+        verify(exactly = 1) {
+            userRepository.insertUser(secondUser)
+        }
+
+    }
+
+    @Test
+    fun `should return throw if userName already exists`() {
+        // given
+        every { userRepository.getUserByUserName(secondUser.username) } returns secondUser
+
+        //when & then
+        assertThrows<UserAlreadyExistException> {
+            createUserUseCase.createUser(secondUser.username,secondUser.password, secondUser.password, secondUser.role)
+        }
+
+    }
+
+    @Test
+    fun `should return throw if userName empty`() {
+        // given
+        every { userRepository.getUserByUserName(secondUser.username) } returns null
+
+        //when & then
+        assertThrows<UserNameEmptyException> {
+            createUserUseCase.createUser("",secondUser.password, secondUser.password, secondUser.role)
+        }
+
+    }
+
+    @Test
+    fun `should return throw if password length less than 6 `() {
+        // given
+        every { userRepository.getUserByUserName(secondUser.username) } returns null
+
+        //when & then
+        assertThrows<InvalidLengthPasswordException> {
+            createUserUseCase.createUser(secondUser.username, "1234",secondUser.password, secondUser.role)
+        }
+
+    }
+
+
+    @Test
+    fun `should return throw if password not equal confirm password `() {
+        // given
+        every { userRepository.getUserByUserName(secondUser.username) } returns null
+
+        //when & then
+        assertThrows<InvalidConfirmPasswordException> {
+            createUserUseCase.createUser(secondUser.username, secondUser.password,"invalid-confirm", secondUser.role)
+        }
+
+    }
+}
