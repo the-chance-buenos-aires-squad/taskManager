9d0c556b5e968095163518e2e44bb0ac8c9c7fbd
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-05-01 09:59:56 +0300

    implement authentication usecase

--- src/main/kotlin/domain/usecases/AuthenticationUseCase.kt
+++ src/main/kotlin/domain/usecases/AuthenticationUseCase.kt
@@ -1,17 +1,36 @@
 package domain.usecases
 
+import domain.customeExceptions.InvalidCredentialsException
+import domain.customeExceptions.UserNameEmptyException
 import domain.entities.User
-import domain.entities.UserRole
 import domain.repositories.UserRepository
-import java.time.LocalDateTime
-import java.util.UUID
+import domain.util.MD5Hash
 
 class AuthenticationUseCase(private val repository: UserRepository) {
 
     fun login(username: String, password: String): User {
-        //todo return user
-        return User(id = UUID.randomUUID().toString(), username = username, password = password , role = UserRole.ADMIN, createdAt = LocalDateTime.now())
+        validateUsername(username)
+        val user = getUserOrThrow(username)
+        checkHashPassword(user, password)
+        return user
     }
 
+    private fun validateUsername(username: String) {
+        if (username.isBlank()) {
+            throw UserNameEmptyException()
+        }
+    }
+
+    private fun getUserOrThrow(username: String): User {
+        return repository.getUserByUserName(username.trim())
+            ?: throw InvalidCredentialsException()
+    }
+
+    private fun checkHashPassword(user: User, inputPassword: String) {
+        val hashedInput = MD5Hash.hash(inputPassword)
+        if (user.password != hashedInput) {
+            throw InvalidCredentialsException()
+        }
+    }
 
 }
