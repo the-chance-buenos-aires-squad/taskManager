12188b8f414d32b91084dc1cd2bec51c6fd6200c
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-05-01 07:38:33 +0300

    add MD5 hash

--- src/test/kotlin/domain/usecases/CreateUserUseCaseTest.kt
+++ src/test/kotlin/domain/usecases/CreateUserUseCaseTest.kt
@@ -1,10 +1,12 @@
 package domain.usecases
 
+import com.google.common.truth.Truth.assertThat
 import domain.customeExceptions.InvalidConfirmPasswordException
 import domain.customeExceptions.InvalidLengthPasswordException
 import domain.customeExceptions.UserAlreadyExistException
 import domain.customeExceptions.UserNameEmptyException
 import domain.repositories.UserRepository
+import domain.util.MD5Hash
 import dummyData.DummyUser
 import io.mockk.every
 import io.mockk.mockk
@@ -33,7 +35,12 @@ class CreateUserUseCaseTest {
 
         //then
         verify(exactly = 1) {
-            userRepository.insertUser(firstUser)
+            userRepository.insertUser(
+                match { user ->
+                    user.username == firstUser.username &&
+                            user.role == firstUser.role
+                }
+            )
         }
 
     }
@@ -53,7 +60,11 @@ class CreateUserUseCaseTest {
 
         //then
         verify(exactly = 1) {
-            userRepository.insertUser(secondUser)
+            userRepository.insertUser(
+                match { user ->
+                    user.role == secondUser.role
+                }
+            )
         }
 
     }
@@ -65,7 +76,7 @@ class CreateUserUseCaseTest {
 
         //when & then
         assertThrows<UserAlreadyExistException> {
-            createUserUseCase.createUser(secondUser.username,secondUser.password, secondUser.password, secondUser.role)
+            createUserUseCase.createUser(secondUser.username, secondUser.password, secondUser.password, secondUser.role)
         }
 
     }
@@ -77,7 +88,7 @@ class CreateUserUseCaseTest {
 
         //when & then
         assertThrows<UserNameEmptyException> {
-            createUserUseCase.createUser("",secondUser.password, secondUser.password, secondUser.role)
+            createUserUseCase.createUser("", secondUser.password, secondUser.password, secondUser.role)
         }
 
     }
@@ -89,7 +100,7 @@ class CreateUserUseCaseTest {
 
         //when & then
         assertThrows<InvalidLengthPasswordException> {
-            createUserUseCase.createUser(secondUser.username, "1234",secondUser.password, secondUser.role)
+            createUserUseCase.createUser(secondUser.username, "1234", secondUser.password, secondUser.role)
         }
 
     }
@@ -102,8 +113,47 @@ class CreateUserUseCaseTest {
 
         //when & then
         assertThrows<InvalidConfirmPasswordException> {
-            createUserUseCase.createUser(secondUser.username, secondUser.password,"invalid-confirm", secondUser.role)
+            createUserUseCase.createUser(secondUser.username, secondUser.password, "invalid-confirm", secondUser.role)
         }
 
     }
+
+
+    @Test
+    fun `should hash password using MD5 utility`() {
+        // Given
+        val password = "test123"
+        val expectedHash = MD5Hash.hash(password)
+
+        // When
+        val actualHash = MD5Hash.hash(password)
+
+        // Then
+        assertThat(actualHash).isEqualTo(expectedHash)
+    }
+
+
+    @Test
+    fun `created user should have hashed password`() {
+        // Arrange
+        val rawPassword = "rawPassword123"
+        every { userRepository.getUserByUserName(any()) } returns null
+
+        // Act
+        createUserUseCase.createUser(
+            username = "newUser",
+            password = rawPassword,
+            confirmPassword = rawPassword
+        )
+
+        // Assert
+        verify {
+            userRepository.insertUser(
+                match { user ->
+                    user.password == MD5Hash.hash(rawPassword)
+                }
+            )
+        }
+    }
+
 }
--- src/test/kotlin/dummyData/DummyUser.kt
+++ src/test/kotlin/dummyData/DummyUser.kt
@@ -1,19 +1,20 @@
 package dummyData
 
 import domain.entities.UserRole
+import domain.util.MD5Hash
 
 object DummyUser {
      val testUserOne = createDummyUser(
         id = "1",
         username = "testUser1",
-        password = "testPassword1",
+        password = MD5Hash.hash("testPassword1"),
         role = UserRole.ADMIN
     )
 
      val testUserTwo = createDummyUser(
         id = "2",
         username = "testUser2",
-        password = "testPassword2",
+        password = MD5Hash.hash("testPassword2"),
         role = UserRole.MATE
     )
 }
