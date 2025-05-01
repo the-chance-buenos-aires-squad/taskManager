9a82a9c39ca2c6d43e2e394778d92ab1297cafa9
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-05-01 11:37:32 +0300

    update data model - used qualifier to inject the correct file

--- src/main/kotlin/di/DataModel.kt
+++ src/main/kotlin/di/DataModel.kt
@@ -1,30 +1,32 @@
 package di
 
-
-//import org.example.data.CsvFileReader
-//import org.example.data.CsvRecipesRepository
-//import org.example.data.MemoryDataSource
-//import org.example.data.RecipeParser
-//import org.example.logic.RecipesRepository
 import com.github.doyaaaaaken.kotlincsv.client.CsvReader
 import com.github.doyaaaaaken.kotlincsv.client.CsvWriter
-import data.util.CsvHandler
+import data.dataSource.util.CsvHandler
+import org.koin.core.qualifier.Qualifier
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import java.io.File
 
-//
+
 val dataModule = module {
     single { CsvWriter() }
     single { CsvReader() }
     single { CsvHandler(get(), get()) }
 
 
-//    single { UserCsvDataSource(Files.UserFile,get()) }
+    single<File>(qualifier = Paths.UserFileQualifier) {
+        File(Paths.UserFilePath)
+    }
 
 
 }
 
-object Files {
-    val UserFile = File("src/main/kotlin/data/resource/users_file.csv")
-
+object Paths {
+    /*
+    when injecting the file to data source use qualifier before injecting
+    @Named("UserFilePath") private val userFile: File
+     */
+    const val UserFilePath = "src/main/kotlin/data/resource/users_file.csv"
+    val UserFileQualifier: Qualifier = named("UserFilePath")
 }
