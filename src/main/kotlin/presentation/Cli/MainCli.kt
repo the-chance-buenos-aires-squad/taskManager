af819082482a58f662ea072d68b046ea2af1313e
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-05-01 07:41:03 +0300

    add ui controller and dashboard screen (#50)

---
+++ src/main/kotlin/presentation/Cli/MainCli.kt
@@ -0,0 +1,38 @@
+package presentation.Cli
+
+import presentation.UiController
+
+class MainCli(private val uiController: UiController, private val loginCli: LoginCli) {
+
+    fun startCli(isTest: Boolean = false) {
+
+        while (true) {
+            uiController.printMessage(
+                "========================================\n" +
+                        "              Welcome to PlanMate!       \n" +
+                        "========================================\n" +
+                        "1. Login\n" +
+                        "2. Exit\n" +
+                        "Choose an option (1 - 2): "
+            )
+            when (uiController.readInput().trim()) {
+
+                "1" -> loginCli.start()
+                "2" -> {
+                    uiController.printMessage("Exiting PlanMate... Goodbye!")
+                    break
+                }
+
+                else -> uiController.printMessage("Invalid option! Please try again.")
+
+
+            }
+
+            if (isTest) {
+                break
+            }
+
+        }
+
+    }
+}
--- src/main/kotlin/presentation/Main.kt
+++ src/main/kotlin/presentation/Main.kt
@@ -1,5 +1,15 @@
 package org.buinos.presentation
 
+import org.koin.java.KoinJavaComponent.getKoin
+import presentation.Cli.LoginCli
+import presentation.Cli.MainCli
+import presentation.UiController
+
 fun main() {
     println("Hello World!")
+
+    val uiController = UiController()
+    val loginCli = LoginCli(uiController)
+    val holderCli = MainCli(uiController, loginCli)
+    holderCli.startCli()
 }
