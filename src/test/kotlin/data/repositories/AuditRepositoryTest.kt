7bf6fa6a9b51eede1fbbfaa5d16c1cf2b4b12e4e
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-05-01 15:58:49 +0300

    adding repository tests

--- src/test/kotlin/data/repositories/AuditRepositoryTest.kt
+++ src/test/kotlin/data/repositories/AuditRepositoryTest.kt
@@ -1,25 +1,48 @@
 package data.repositories
 
+import com.google.common.truth.Truth.assertThat
 import data.dataSource.CsvAuditDataSource
+import data.dummyData.DummyAudits
+import io.mockk.every
 import io.mockk.mockk
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class AuditRepositoryTest{
+class AuditRepositoryTest {
 
-  private val mockedDataSource = mockk<CsvAuditDataSource>(relaxed = true)
-  private lateinit var auditRepository: AuditRepository
-
-
-  @BeforeEach
-  fun setUp(){
-   auditRepository = AuditRepository(mockedDataSource)
-  }
+    private val mockedDataSource = mockk<CsvAuditDataSource>(relaxed = true)
+    private lateinit var auditRepository: AuditRepository
 
 
+    @BeforeEach
+    fun setUp() {
+        auditRepository = AuditRepository(mockedDataSource)
+    }
 
 
 
+    @Test
+    fun `should return true when add audit successful in the data source`(){
+        //given
+        every { mockedDataSource.addAudit(DummyAudits.dummyProjectAudit_CreateAction) } returns true
 
- }
+        //when
+        val result = auditRepository.addAudit(DummyAudits.dummyProjectAudit_CreateAction)
+
+        //then
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `should return false when add audit unSuccessful in the data source`(){
+        //given
+        every { mockedDataSource.addAudit(DummyAudits.dummyProjectAudit_CreateAction) } returns false
+
+        //when
+        val result = auditRepository.addAudit(DummyAudits.dummyProjectAudit_CreateAction)
+
+        //then
+        assertThat(result).isFalse()
+    }
+
+}
