46926788fffcf99076ea53a9183fad384b99813c
moyasser hazim <measer100@gmail.com>
2025-05-02 20:15:17 +0300

    inject data source and mapper in constructor

--- src/main/kotlin/data/repositories/TaskRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/TaskRepositoryImpl.kt
@@ -1,17 +1,19 @@
 package data.repositories
 
+import data.dataSource.task.TaskDataSource
+import data.repositories.mappers.Mapper
 import domain.entities.Task
 import domain.repositories.TaskRepository
 import java.time.LocalDateTime
 import java.util.*
 
-class TaskRepositoryImpl:TaskRepository {
+class TaskRepositoryImpl(private val csvTaskDataSource: TaskDataSource,private val taskMapper: Mapper<Task>) :
+    TaskRepository {
 
     override fun createTask(task: Task): Task {
-       return task.copy(
-            id = UUID.randomUUID(),
-            createdAt = LocalDateTime.now(),
-            updatedAt = LocalDateTime.now()
-        )
+        val newTask = task.copy(id = UUID.randomUUID(),createdAt = LocalDateTime.now(),updatedAt = LocalDateTime.now())
+        val mappedTask = taskMapper.mapEntityToRow(newTask)
+        csvTaskDataSource.addTask(mappedTask)
+        return task
     }
 }
