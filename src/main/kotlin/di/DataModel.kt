116c18d76c6dd8e3da553153740c96016f440af1
moyasser hazim <measer100@gmail.com>
2025-05-02 20:17:25 +0300

    add task repository di

--- src/main/kotlin/di/DataModel.kt
+++ src/main/kotlin/di/DataModel.kt
@@ -3,12 +3,10 @@ package di
 
 import com.github.doyaaaaaken.kotlincsv.client.CsvReader
 import com.github.doyaaaaaken.kotlincsv.client.CsvWriter
-
 import data.dataSource.user.CsvUserDataSource
 import data.dataSource.user.UserDataSource
-
-
 import data.dataSource.util.CsvHandler
+import data.repositories.TaskRepositoryImpl
 import data.repositories.UserRepositoryImpl
 import data.repositories.mappers.Mapper
 import data.repositories.mappers.UserMapper
@@ -18,14 +16,15 @@ import org.koin.core.qualifier.Qualifier
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import java.io.File
-import kotlin.io.path.Path
 
 
 //
 val dataModule = module {
     single { CsvWriter() }
     single { CsvReader() }
-    single { CsvHandler(get(), get()) }
+    single { CsvHandler(get(),get()) }
+    single { CsvUserDataSource(get(),get(Paths.UserFileQualifier)) }
+    single { TaskRepositoryImpl(get(),get()) }
 
 
     single<File>(qualifier = Paths.UserFileQualifier) {
