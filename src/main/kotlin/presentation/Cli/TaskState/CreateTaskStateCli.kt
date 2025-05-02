59a66eefaeed3fa63f63f3596dbd4f35840a52e1
NadeenRR <nadeenradwan0@gmail.com>
2025-05-02 18:06:47 +0300

    - add implementation for createTaskState fun in CreateTaskStateCli

--- src/main/kotlin/presentation/Cli/TaskState/CreateTaskStateCli.kt
+++ src/main/kotlin/presentation/Cli/TaskState/CreateTaskStateCli.kt
@@ -1,6 +1,7 @@
 package presentation.Cli.TaskState
 
 import domain.Validator.TaskStateInputValidator
+import domain.entities.TaskState
 import domain.usecases.CreateTaskStateUseCase
 import presentation.UiController
 
@@ -9,5 +10,29 @@ class CreateTaskStateCli(
     private val uiController: UiController,
     private val inputValidator: TaskStateInputValidator
 ) {
-    fun createTaskState() {}
+    fun createTaskState() {
+        val taskState = readAndValidateUserInput()
+        val result = createTaskStateUseCase.execute(taskState)
+
+        uiController.printMessage(
+            if (result) "Task state created successfully."
+            else "Failed to create task state."
+        )
+    }
+
+
+    private fun readAndValidateUserInput(): TaskState {
+        uiController.printMessage("Enter task state ID:")
+        val id = uiController.readInput().trim()
+
+        uiController.printMessage("Enter task state name:")
+        val name = uiController.readInput().trim()
+
+        uiController.printMessage("Enter project ID:")
+        val projectId = uiController.readInput().trim()
+
+        inputValidator.validate(id, name, projectId)
+
+        return TaskState(id = id, name = name, projectId = projectId)
+    }
 }
