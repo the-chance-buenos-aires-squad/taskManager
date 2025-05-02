e575f605a745179fd4be2efa4e36ec80624337a5
moyasser hazim <measer100@gmail.com>
2025-05-02 20:12:43 +0300

    refactor Task data class to set default values

--- src/main/kotlin/domain/entities/Task.kt
+++ src/main/kotlin/domain/entities/Task.kt
@@ -3,14 +3,12 @@ package domain.entities
 import java.time.LocalDateTime
 import java.util.*
 
-data class Task(
-    val id: UUID = UUID.randomUUID(),
+data class Task(val id: UUID = UUID.randomUUID(),
     val title: String,
     val description: String,
     val projectId: UUID,
     val stateId: UUID,
     val assignedTo: UUID?,
     val createdBy: UUID,
-    val createdAt: LocalDateTime,
-    val updatedAt: LocalDateTime,
-)
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val updatedAt: LocalDateTime = LocalDateTime.now())
