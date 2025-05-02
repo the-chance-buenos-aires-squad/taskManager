da68c70b7e1fb363340117cd5dd4c82db31640bb
moyasser hazim <measer100@gmail.com>
2025-05-02 20:16:45 +0300

    refactor to use uuid instead of string

--- src/main/kotlin/data/repositories/mappers/TaskMapper.kt
+++ src/main/kotlin/data/repositories/mappers/TaskMapper.kt
@@ -5,31 +5,28 @@ import java.time.LocalDateTime
 import java.util.*
 
 class TaskMapper : Mapper<Task> {
+
     override fun mapEntityToRow(entity: Task): List<String> {
-        return listOf(
-            entity.id.toString(),
+        return listOf<String>(entity.id.toString(),
             entity.title,
             entity.description,
-            entity.projectId,
-            entity.stateId,
-            entity.assignedTo.orEmpty(),
-            entity.createdBy,
+            entity.projectId.toString(),
+            entity.stateId.toString(),
+            entity.assignedTo.toString(),
+            entity.createdBy.toString(),
             entity.createdAt.toString(),
-            entity.updatedAt.toString()
-        )
+            entity.updatedAt.toString())
     }
 
     override fun mapRowToEntity(row: List<String>): Task {
-        return Task(
-            id = UUID.fromString(row[0]),
+        return Task(id = UUID.fromString(row[0]),
             title = row[1],
             description = row[2],
-            projectId = row[3],
-            stateId = row[4],
-            assignedTo = row[5].ifBlank { null },
-            createdBy = row[6],
+            projectId = UUID.fromString(row[3]),
+            stateId = UUID.fromString(row[4]),
+            assignedTo = UUID.fromString(row[5].ifBlank { null }),
+            createdBy = UUID.fromString(row[6]),
             createdAt = LocalDateTime.parse(row[7]),
-            updatedAt = LocalDateTime.parse(row[8])
-        )
+            updatedAt = LocalDateTime.parse(row[8]))
     }
 }
