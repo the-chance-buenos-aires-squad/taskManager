f13a6b34dbd6ff9d4f1c7307c8e2173ce355d9be
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-05-03 04:53:57 +0300

    implement create user cli

--- src/main/kotlin/presentation/Cli/auth/CreateUserCli.kt
+++ src/main/kotlin/presentation/Cli/auth/CreateUserCli.kt
@@ -9,13 +9,30 @@ class CreateUserCli(
     private val createUserUseCase: CreateUserUseCase
 ) {
 
-    fun start() {}
+    fun start() {
+        uiController.printMessage(HEADER_MESSAGE)
+
+        uiController.printMessage(USERNAME_PROMPT_MESSAGE)
+        val username = uiController.readInput().trim()
+
+        uiController.printMessage(PASSWORD_PROMPT_MESSAGE)
+        val password = uiController.readInput().trim()
+
+        uiController.printMessage(CONFIRM_PASSWORD_PROMPT_MESSAGE)
+        val confirmPassowrd = uiController.readInput().trim()
+        try {
+            val newUserMate = createUserUseCase.addUser(username, password, confirmPassowrd)
+            uiController.printMessage(SUCCESS_MESSAGE.format(newUserMate.username))
+        } catch (e: Exception) {
+            uiController.printMessage(ERROR_MESSAGE.format(e.message))
+        }
+
+    }
 
     companion object {
         const val HEADER_MESSAGE = "========================================\n" +
                 "              Create New User           \n" +
                 "========================================\n"
-
         const val USERNAME_PROMPT_MESSAGE = "username: "
         const val PASSWORD_PROMPT_MESSAGE = "password: "
         const val CONFIRM_PASSWORD_PROMPT_MESSAGE = "confirm password: "
