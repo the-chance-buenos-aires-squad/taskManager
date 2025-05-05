a3823cf8ad376c54be8cf46651279bf340ec9b97
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-05-05 11:20:13 +0300

    implement update task use case with tests

---
+++ src/main/kotlin/domain/usecases/task/UpdateTaskUseCase.kt
@@ -0,0 +1,53 @@
+package domain.usecases.task
+
+import domain.customeExceptions.UserNotLoggedInException
+import domain.entities.ActionType
+import domain.entities.EntityType
+import domain.entities.Task
+import domain.repositories.AuthRepository
+import domain.repositories.TaskRepository
+import domain.usecases.AddAuditUseCase
+import java.util.*
+
+class UpdateTaskUseCase(
+    private val taskRepository: TaskRepository,
+    private val addAuditUseCase: AddAuditUseCase,
+    private val authRepository: AuthRepository
+) {
+    fun updateTask(
+        id: UUID,
+        title: String,
+        description: String,
+        projectId: UUID,
+        stateId: UUID,
+        assignedTo: UUID?=null,
+        createdBy: UUID
+    ): Boolean {
+        val updatedTask = Task(
+            id = id,
+            title = title,
+            description = description,
+            projectId = projectId,
+            stateId = stateId,
+            assignedTo = assignedTo,
+            createdBy = createdBy,
+        )
+
+        val currentUser = authRepository.getCurrentUser()
+            ?: throw UserNotLoggedInException()
+
+        return taskRepository.updateTask(updatedTask).also { result->
+            if (result){
+                addAuditUseCase.addAudit(
+                    entityId = updatedTask.id.toString(),
+                    entityType = EntityType.TASK,
+                    action = ActionType.UPDATE,
+                    field = "",
+                    oldValue = "${updatedTask.id}",
+                    newValue = "$updatedTask",
+                    userId = currentUser.id.toString()
+                )
+            }
+        }
+    }
+}
