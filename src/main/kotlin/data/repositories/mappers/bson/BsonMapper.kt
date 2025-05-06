ed3c7937f331d3f0e3dcbe8abb1a05da4500d3ad
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-05-06 19:11:12 +0300

    delete mapper classes

--- src/main/kotlin/data/repositories/mappers/bson/BsonMapper.kt
+++
@@ -1,8 +0,0 @@
-package data.repositories.mappers.bson
-
-import org.bson.Document
-
-interface BsonMapper<entity> {
-    fun toDocument(entity: entity): Document
-    fun fromDocument(document: Document): entity
-}
