193b48fcdc2801086abcccfa7455a44c5fb3a428
moyasser hazim <measer100@gmail.com>
2025-05-07 01:11:02 +0300

    make TaskDataSource methods suspend functions for coroutine support

--- src/main/kotlin/data/dataSource/task/TaskDataSource.kt
+++ src/main/kotlin/data/dataSource/task/TaskDataSource.kt
@@ -1,9 +1,10 @@
 package data.dataSource.task
 
 interface TaskDataSource {
-    fun addTask(taskRow: List<String>): Boolean
-    fun getTasks(): List<List<String>>
-    fun getTaskById(taskId: String): List<String>?
-    fun deleteTask(taskId: String): Boolean
-    fun updateTask(taskRow: List<String>): Boolean
+
+    suspend fun addTask(taskRow: List<String>): Boolean
+    suspend fun getTasks(): List<List<String>>
+    suspend fun getTaskById(taskId: String): List<String>?
+    suspend fun deleteTask(taskId: String): Boolean
+    suspend fun updateTask(taskRow: List<String>): Boolean
 }
