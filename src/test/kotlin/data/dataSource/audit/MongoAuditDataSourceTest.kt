78284742e56c6db8095dbdcf445a3bf5a9d37e46
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-05-08 19:14:00 +0300

    implement mongo data source tests

--- src/test/kotlin/data/dataSource/audit/MongoAuditDataSourceTest.kt
+++ src/test/kotlin/data/dataSource/audit/MongoAuditDataSourceTest.kt
@@ -1,31 +1,66 @@
 package data.dataSource.audit
 
 import com.google.common.truth.Truth.assertThat
+import com.mongodb.client.result.InsertOneResult
+import com.mongodb.kotlin.client.coroutine.FindFlow
+import com.mongodb.kotlin.client.coroutine.MongoCollection
+import com.mongodb.reactivestreams.client.FindPublisher
 import data.dto.AuditDto
 import data.dummyData.DummyAudits.DummyTaskAuditDto
+import di.MongoCollections
+import io.mockk.*
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class MongoAuditDataSourceTest{
 
-    private  var  dataSource : FakeMongoAuditDataSource= FakeMongoAuditDataSource()
+    private lateinit var  dataSource : MongoAuditDataSource
+    private  var  auditCollection: MongoCollection<AuditDto> = mockk(relaxed = true)
     private  var testAudit: AuditDto = DummyTaskAuditDto
 
-    @Test
-    fun `addAudit should return true`() = runTest {
-        val added = dataSource.addAudit(testAudit)
-        assertThat(added).isTrue()
+    @BeforeEach
+    fun setUp(){
+        dataSource = MongoAuditDataSource(auditCollection)
     }
+
+
+    @Test
+    fun `addAudit should call collection insertOne`() = runTest {
+        // Arrange
+        val mockResult = mockk<InsertOneResult>()
+        every { mockResult.wasAcknowledged() } returns true
+
+        coEvery { auditCollection.insertOne(document = testAudit, options = any()) } returns mockResult
+
+        // Act
+        val result = dataSource.addAudit(testAudit)
+
+        // Assert
+        coVerify { auditCollection.insertOne(testAudit,any()) }
+        coEvery { auditCollection.insertOne(testAudit,any()).wasAcknowledged() }
+        assertThat(result).isTrue()
+    }
+
 
     @Test
     fun `getAllAudit should return all Audit`() = runTest {
-        dataSource.addAudit(testAudit)
-        dataSource.addAudit(testAudit)
+        val expectedAudits = listOf(testAudit, testAudit)
+
+
+        val mockFindFlow = mockk<FindFlow<AuditDto>>(relaxed = true)
+        coEvery { mockFindFlow.toList() } returns expectedAudits
+
+        coEvery { auditCollection.find() } returns mockFindFlow
+        coEvery { auditCollection.find().toList() } returns expectedAudits
+
 
         val result = dataSource.getAllAudit()
-        assertThat(result).hasSize(2)
-        assertThat(result).containsExactly(testAudit, testAudit)
-    }
 
+
+        coVerify { auditCollection.find() }
+    }
 
 }
