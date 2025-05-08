c995a1c6eedcfab80eb0231e2e3654807d8c7bbd
moyasser hazim <measer100@gmail.com>
2025-05-08 19:55:29 +0300

    refactor repositoryModule to update TaskRepositoryImpl to use TaskDataSource

--- src/main/kotlin/domain/repositories/TaskRepository.kt
+++ src/main/kotlin/domain/repositories/TaskRepository.kt
@@ -4,9 +4,10 @@ import domain.entities.Task
 import java.util.*
 
 interface TaskRepository {
-    fun addTask(task: Task): Boolean
-    fun getAllTasks(): List<Task>
-    fun getTaskById(id: UUID): Task?
-    fun updateTask(task: Task): Boolean
-    fun deleteTask(id: UUID): Boolean
+
+    suspend fun addTask(task: Task): Boolean
+    suspend fun getAllTasks(): List<Task>
+    suspend fun getTaskById(id: UUID): Task?
+    suspend fun updateTask(task: Task): Boolean
+    suspend fun deleteTask(id: UUID): Boolean
 }
