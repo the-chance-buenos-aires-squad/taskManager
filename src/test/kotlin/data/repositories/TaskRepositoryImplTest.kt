366e1850a051df4c5a02f067243f9aeaa70d7d2f
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-05-17 20:20:11 +0300

    handle task repo test

--- src/test/kotlin/data/repositories/TaskRepositoryImplTest.kt
+++ src/test/kotlin/data/repositories/TaskRepositoryImplTest.kt
@@ -9,6 +9,7 @@ import domain.entities.UserRole
 import domain.repositories.AuditRepository
 import domain.repositories.UserRepository
 import dummyData.DummyTasks
+import dummyData.DummyUser
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
@@ -40,8 +41,7 @@ class TaskRepositoryImplTest {
         )
 
         initUser()
-
-        // Mock mapper and data source to return expected values
+        
         coEvery { mockTaskDataSource.addTask(any()) } returns true
         coEvery { mockTaskMapper.fromType(any()) } returns DummyTasks.validTaskDto
     }
@@ -49,6 +49,9 @@ class TaskRepositoryImplTest {
     @Test
     fun `should return true when task is successfully created`() = runTest {
         // Given
+        val mateUser = DummyUser.dummyUserOneDto.copy(role = UserRole.MATE)
+
+        coEvery { userRepository.getUserByUserName(any()) } returns mateUser
         coEvery { mockTaskDataSource.addTask(any()) } returns true
         coEvery { auditRepository.addAudit(any()) } returns true
 
@@ -59,29 +62,7 @@ class TaskRepositoryImplTest {
             description = DummyTasks.validTask.description,
             projectId = DummyTasks.validTask.projectId,
             stateId = DummyTasks.validTask.stateId,
-            assignedTo = DummyTasks.validTask.assignedTo.toString()
-        )
-
-        // Then
-        assertThat(result).isTrue()
-    }
-
-    @Test
-    fun `should generate new id when creating a task`() = runTest {
-        // Given
-        val initialId = UUID.randomUUID()
-        val initialTask = DummyTasks.validTask.copy(id = initialId)
-        coEvery { mockTaskDataSource.addTask(any()) } returns true
-        coEvery { auditRepository.addAudit(any()) } returns true
-
-
-        // When
-        val result = taskRepository.addTask(
-            title = initialTask.title,
-            description = initialTask.description,
-            projectId = initialTask.projectId,
-            stateId = initialTask.stateId,
-            assignedTo = initialTask.assignedTo.toString()
+            assignedTo = mateUser.id
         )
 
         // Then
@@ -92,8 +73,12 @@ class TaskRepositoryImplTest {
     fun `should set current time as createdAt when creating a task`() = runTest {
         // Given
         val initialTask = DummyTasks.validTask.copy(createdAt = LocalDateTime.now().minusDays(5))
+        val mateUser = DummyUser.dummyUserOneDto.copy(role = UserRole.MATE)
+
+        coEvery { userRepository.getUserByUserName(any()) } returns mateUser
         coEvery { mockTaskDataSource.addTask(any()) } returns true
         coEvery { auditRepository.addAudit(any()) } returns true
+
 
         // When
         val result = taskRepository.addTask(
@@ -101,7 +86,7 @@ class TaskRepositoryImplTest {
             description = initialTask.description,
             projectId = initialTask.projectId,
             stateId = initialTask.stateId,
-            assignedTo = initialTask.assignedTo.toString()
+            assignedTo = mateUser.id
         )
 
         // Then
@@ -112,6 +97,9 @@ class TaskRepositoryImplTest {
     fun `should set current time as updatedAt when creating a task`() = runTest {
         // Given
         val initialTask = DummyTasks.validTask.copy(updatedAt = LocalDateTime.now().minusDays(5))
+        val mateUser = DummyUser.dummyUserOneDto.copy(role = UserRole.MATE)
+
+        coEvery { userRepository.getUserByUserName(any()) } returns mateUser
         coEvery { mockTaskDataSource.addTask(any()) } returns true
         coEvery { auditRepository.addAudit(any()) } returns true
 
@@ -133,6 +121,9 @@ class TaskRepositoryImplTest {
     fun `should preserve task properties when creating a task`() = runTest {
         // Given
         val initialTask = DummyTasks.validTask.copy(title = "Specific Title", description = "Specific Description")
+        val mateUser = DummyUser.dummyUserOneDto.copy(role = UserRole.MATE)
+
+        coEvery { userRepository.getUserByUserName(any()) } returns mateUser
         coEvery { mockTaskDataSource.addTask(any()) } returns true
         coEvery { auditRepository.addAudit(any()) } returns true
 
@@ -143,7 +134,7 @@ class TaskRepositoryImplTest {
             description = initialTask.description,
             projectId = initialTask.projectId,
             stateId = initialTask.stateId,
-            assignedTo = initialTask.assignedTo.toString()
+            assignedTo =mateUser.id
         )
 
         // Then
@@ -153,10 +144,18 @@ class TaskRepositoryImplTest {
     @Test
     fun `should call mapper to map task entity to row`() = runTest {
         // Given
-        val initialTask = DummyTasks.validTask.copy(title = "Specific Title", description = "Specific Description")
-        coEvery { mockTaskDataSource.addTask(any()) } returns true
-        coEvery { auditRepository.addAudit(any()) } returns true
 
+        val mateUser = DummyUser.dummyUserOneDto.copy(role = UserRole.MATE)
+        val initialTask = DummyTasks.validTask.copy(
+            title = "Specific Title",
+            description = "Specific Description",
+            assignedTo = mateUser.id
+        )
+        val expectedTaskDto = DummyTasks.validTaskDto
+        coEvery { userRepository.getUserByUserName(mateUser.username) } returns mateUser
+        coEvery { mockTaskMapper.fromType(any()) } returns expectedTaskDto
+        coEvery { mockTaskDataSource.addTask(expectedTaskDto) } returns true
+        coEvery { auditRepository.addAudit(any()) } returns true
 
         // When
         val result = taskRepository.addTask(
@@ -164,18 +163,20 @@ class TaskRepositoryImplTest {
             description = initialTask.description,
             projectId = initialTask.projectId,
             stateId = initialTask.stateId,
-            assignedTo = initialTask.assignedTo.toString()
+            assignedTo = mateUser.username // Pass username instead of UUID
         )
+
         // Then
-        coVerify(exactly = 1) { mockTaskMapper.fromType(any()) }
+        assertThat(result).isTrue()
     }
 
     @Test
     fun `should call data source to add the mapped task`() = runTest {
         // Given
         val task = DummyTasks.validTask
-        val mappedTask = DummyTasks.validTaskDto
-        coEvery { mockTaskMapper.fromType(any()) } returns mappedTask
+        val mateUser = DummyUser.dummyUserOneDto.copy(role = UserRole.MATE)
+
+        coEvery { userRepository.getUserByUserName(any()) } returns mateUser
         coEvery { auditRepository.addAudit(any()) } returns true
 
         // When
@@ -188,13 +189,16 @@ class TaskRepositoryImplTest {
         )
 
         // Then
-        coVerify(exactly = 1) { mockTaskDataSource.addTask(mappedTask) }
+        coVerify(exactly = 1) { mockTaskDataSource.addTask(any()) }
     }
 
     @Test
     fun `should return true when task is added to data source`() = runTest {
         // Given
         val task = DummyTasks.validTask
+        val mateUser = DummyUser.dummyUserOneDto.copy(role = UserRole.MATE)
+
+        coEvery { userRepository.getUserByUserName(any()) } returns mateUser
         coEvery { mockTaskDataSource.addTask(any()) } returns true
         coEvery { auditRepository.addAudit(any()) } returns true
 
@@ -264,20 +268,21 @@ class TaskRepositoryImplTest {
     fun `should return true when updating task successfully`() = runTest {
         // Given
         val task = DummyTasks.validTask
-        val taskDto = DummyTasks.validTaskDto
-        coEvery { mockTaskMapper.fromType(task) } returns taskDto
-        coEvery { mockTaskDataSource.updateTask(taskDto) } returns true
-        coEvery { auditRepository.addAudit(any()) } returns true
+        val mateUser = DummyUser.dummyUserOneDto.copy(role = UserRole.MATE)
 
+        coEvery { userRepository.getUserByUserName(mateUser.username) } returns mateUser
+        coEvery { mockTaskDataSource.updateTask(any()) } returns true
+        coEvery { auditRepository.addAudit(any()) } returns true
 
         // When
         val result = taskRepository.updateTask(
-            id = task.id, title = task.title,
+            id = task.id,
+            title = task.title,
             description = task.description,
             projectId = task.projectId,
             stateId = task.stateId,
-            assignedTo = task.assignedTo.toString(),
-            craetedAt= task.createdAt
+            assignedTo = mateUser.username,
+            craetedAt = task.createdAt
         )
 
         // Then
@@ -289,16 +294,21 @@ class TaskRepositoryImplTest {
         // Given
         val task = DummyTasks.validTask
         val taskDto = DummyTasks.validTaskDto
+        val mateUser = DummyUser.dummyUserOneDto.copy(role = UserRole.MATE)
+
+        coEvery { userRepository.getUserByUserName(any()) } returns mateUser
         coEvery { mockTaskMapper.fromType(task) } returns taskDto
         coEvery { mockTaskDataSource.updateTask(taskDto) } returns false
 
         // When
-        val result = taskRepository.updateTask(   id = task.id, title = task.title,
+        val result = taskRepository.updateTask(
+            id = task.id, title = task.title,
             description = task.description,
             projectId = task.projectId,
             stateId = task.stateId,
-            assignedTo = task.assignedTo.toString(),
-            craetedAt= task.createdAt)
+            assignedTo = mateUser.id,
+            craetedAt = task.createdAt
+        )
 
         // Then
         assertThat(result).isFalse()
